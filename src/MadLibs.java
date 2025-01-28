2ad00989d3ffec6fd2f75e294d2298c701cf1740
Horror_Guy7 <ccsimmons28@riverdale.edu>
2025-01-28 08:38:52 -0800

    assignment

---
+++ src/MadLibs.java
@@ -0,0 +1,8 @@
+public class MadLibs {
+    public static void main(String[] args) {
+        System.out.println(args[0] + ", an amazing athlete, met with " + args[1] + ",and they are besties. Once, they were "
+                + args[2] + " some delicious " + args[3] + " " + args[4] + " and soon they " +
+                args[5] + " something cool at " + args[6] + ", which sells " + args[7] + "\n" + args[8] + " " + args[9] + " " + args[10]);
+    }
+
+}
---
+++ src/README.md
@@ -0,0 +1,15 @@
+This is the documentation for the mad libs.
+
+After compiling java, you can go to the class's arguments and type the following using spaces between words:
+
+1. An athlete
+2. A celebrity's last name
+3. A verb ending in ing
+4.  An adjective
+5. A food item
+6. A past tense verb
+7. A place
+8. A plural noun
+9.  A Noun
+10. A verb
+11. A 
