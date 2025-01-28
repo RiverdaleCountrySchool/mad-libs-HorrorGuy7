14698d1b393d59874fe8540c34dd4734c28e626f
Horror_Guy7 <ccsimmons28@riverdale.edu>
2025-01-28 08:42:45 -0800

    assignment

--- src/MadLibs.java
+++ src/MadLibs.java
@@ -2,7 +2,7 @@ public class MadLibs {
     public static void main(String[] args) {
         System.out.println(args[0] + ", an amazing athlete, met with " + args[1] + ",and they are besties. Once, they were "
                 + args[2] + " some delicious " + args[3] + " " + args[4] + " and soon they " +
-                args[5] + " something cool at " + args[6] + ", which sells " + args[7] + "\n" + args[8] + " " + args[9] + " " + args[10]);
+                args[5] + " something cool at " + args[6] + ", which sells very cool " + args[7] + "\n" + args[8] + " " + args[9] + " " + args[10]);
     }
 
 }
